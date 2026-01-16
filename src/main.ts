e6f119cd0b1bfcb5a28c23692f2b93b1765ce95c
Ethan <136395115+Ethan-bit101@users.noreply.github.com>
2026-01-16 15:35:29 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,23 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-    dotenv.config();
-    const app = await NestFactory.create(AppModule);
-    const port = process.env.PORT || 3000;
-    app.enableCors();
-    await app.listen(+port);
-    console.log(`Server listening on http://localhost:${port}`);
+  dotenv.config();
+
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'https://p8-xi.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`Server running on port ${port}`);
 }
 
 bootstrap();
-
